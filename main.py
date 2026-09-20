3c7cc66cd5f4f24693b376b02cd38eaa02deccf9
Tusharkamthe23 <139905124+Tusharkamthe23@users.noreply.github.com>
2026-09-20 20:22:19 +0530

    Change LLM model name to openai/gpt-oss-20b

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ retriever = db.as_retriever(search_kwargs={"k": 3})
 # --- LLM ---
 llm = ChatGroq(
     api_key=GROQ_API_KEY,
-    model_name="llama-3.3-70b-versatile"
+    model_name="openai/gpt-oss-20b"
 )
 
 # --- Format retrieved docs ---
